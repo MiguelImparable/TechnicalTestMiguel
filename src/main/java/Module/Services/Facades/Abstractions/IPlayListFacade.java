b13432e42e6e857ab03b1ddddf30e28a64d4c960
Miguel Angel Diaz <migerodd888@gmail.com>
2025-01-22 18:01:47 -0500

    Feat(Facades): Add facades

---
+++ src/main/java/Module/Services/Facades/Abstractions/IPlayListFacade.java
@@ -0,0 +1,17 @@
+﻿package Module.Services.Facades.Abstractions;
+
+import java.util.List;
+import Entities.Business.Dto.CreatePlaylistRequest;
+import Entities.Business.Dto.GenericResponse;
+import Entities.Business.Entities.Playlist;
+
+public interface IPlayListFacade {
+    
+    GenericResponse<List<Playlist>> addPlaylist(CreatePlaylistRequest request);
+
+    GenericResponse<List<Playlist>> getAllPlaylists();
+
+    GenericResponse<List<Playlist>> getPlaylistByName(String request);
+
+    GenericResponse<String> deletePlaylist(String request);
+}
---
+++ src/main/java/Module/Services/Facades/PlayListFacade.java
@@ -0,0 +1,39 @@
+﻿package Module.Services.Facades;
+
+import java.util.List;
+
+import Dal.Business.Entities.DbContext;
+import Module.Business.WorkFlow.PlayListWF;
+import Module.Services.Facades.Abstractions.IPlayListFacade;
+import Entities.Business.Dto.CreatePlaylistRequest;
+import Entities.Business.Dto.GenericResponse;
+import Entities.Business.Entities.Playlist;
+
+public class PlayListFacade implements IPlayListFacade {
+
+    private final PlayListWF playListWF;
+
+    public PlayListFacade(DbContext dbContext) {
+        this.playListWF = new PlayListWF(dbContext);
+    }
+
+    @Override
+    public GenericResponse<List<Playlist>> addPlaylist(CreatePlaylistRequest request) {
+        return playListWF.addPlaylist(request);
+    }
+
+    @Override
+    public GenericResponse<List<Playlist>> getAllPlaylists() {
+        return playListWF.getAllPlaylists();
+    }
+
+    @Override
+    public GenericResponse<List<Playlist>> getPlaylistByName(String request) {
+        return playListWF.getPlaylistByName(request);
+    }
+
+    @Override
+    public GenericResponse<String> deletePlaylist(String request) {
+        return playListWF.deletePlaylist(request);
+    }
+}
