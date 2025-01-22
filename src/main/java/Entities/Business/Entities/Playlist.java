32adcd590cdb586dd3495f14e51a7870512bb3f5
Miguel Angel Diaz <migerodd888@gmail.com>
2025-01-22 18:00:22 -0500

    Feat(Entity): Add entity object

---
+++ src/main/java/Entities/Business/Entities/Playlist.java
@@ -0,0 +1,41 @@
+﻿package Entities.Business.Entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Document(collection = "PlayLists")
+public class Playlist {
+
+    @Id 
+    private UUID id;
+
+    private String name;
+
+    public Playlist() {
+    }
+
+    // Constructor con parámetros
+    public Playlist(String name) {
+        this.name = name;
+    }
+
+    // Getters y Setters
+    public Date getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
